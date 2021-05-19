4ea4df8b38438d8d245745a70e97e355a62bec65
christiangerbig <chr_gerbig@web.de>
2021-05-19 15:06:05 +0200

    upade 1.2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Contact from "./components/Contact";
 import NotFound from './components/NotFound';
 
 class App extends Component {
-
   state = {
     error: null
   }
@@ -44,43 +43,9 @@ class App extends Component {
               error: err.response.data.errorMessage
             }
           );
-          console.log("Fehler",err.response.data.errorMessage);
         }
       );
   }
-
-
-  /*handleRequestSubmit = (event) => {
-    event.preventDefault();
-    const {email, subject, message} = event.target;
-    //1. Make an API call to the server side Route to create a request
-    let request = {
-      email: email.value,
-      subject: subject.value,
-      message: message.value
-    };
-    axios.post(`${config.API_URL}/api/request`, request, { withCredentials: true })
-      .then(
-        () => {
-          // 2. Once the server has successfully created a new message, update your state that is visible to the user
-          this.setState(
-            () => {
-              // 3. Once the state is update, redirect the user to the home page
-              this.props.history.push("/");
-            }
-          );
-        }
-      )
-      .catch(
-        (err) => {
-          this.setState(
-            {
-              errorMessage: err.response.data.errorMessage
-            }
-          );
-        }
-      );
-  }*/
 
   // -------------- Render ---------------
   render() {
@@ -108,17 +73,8 @@ class App extends Component {
             (routeProps) => {
               return <Contact onRequest={this.handleRequestSubmit} error={error} {...routeProps} />
             }
-          } />
-
-          {/*<Route path="/contact" render={
-            (routeProps) => {
-              return <Contact onRequest={this.handleRequestSubmit} errorMessage={errorMessage} {...routeProps}/>
-            }
-          }/>*/}
-         
-          {
-           <Route component={NotFound}/>
-          }
+          }/>
+          <Route component={NotFound}/>
         </Switch>
         <Footer/>
       </div>
