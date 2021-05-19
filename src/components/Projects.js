47a5e105f24a43e488637c154790480f58ccc7dd
christiangerbig <chr_gerbig@web.de>
2021-05-19 15:25:37 +0200

    upade 1.2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import letterShooter from "../images/letter-shooter.png";
-// import trackertools from "../images/trackertools.png";
-// import jungleSwap from "../images/jungle-swap.png";
+import jungleSwap from "../images/jungle-swap.png";
+import trackertools from "../images/trackertools.png";
+import letterShooter from "../images/letter-shooter.png";
 
 class Projects extends Component {
   render() {
@@ -14,8 +14,7 @@ class Projects extends Component {
         <div className="projectsContainer">
           <div className="projectDetailsContainer">
             <div>
-              {/*<img src={jungleSwap} alt="app jungleswap screenshot"/>*/}
-              <video width="360" height="200" autoplay muted controls src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-13-45-637566759460450173.mp4"/>
+              <video width="360" height="200" autoplay muted controls poster={jungleSwap} src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-13-45-637566759460450173.mp4"/>
             </div>
             <div>
               <h3> JungleSwap </h3>
@@ -44,8 +43,7 @@ class Projects extends Component {
           </div>
           <div className="projectDetailsContainer">
             <div>
-              {/*<img src={trackertools} alt="app trackertools screenshot"/>*/}  
-              <video width="360" height="200" autoplay muted controls src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-13-35-637566753722531829.mp4"/>         
+              <video width="360" height="200" autoplay muted controls poster={trackertools} src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-13-35-637566753722531829.mp4"/>         
             </div>
             <div>
               <h3> Trackertools </h3>
@@ -71,8 +69,7 @@ class Projects extends Component {
           </div>
           <div className="projectDetailsContainer">
             <div>
-              {/*<img src={letterShooter} alt="app lettershooter screenshot"/>*/}
-              <video width="360" height="200" autoplay muted controls src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-14-10-637566774755686276.mp4"/>
+              <video width="360" height="200" autoplay muted controls poster={letterShooter} src="https://bliskcloudstorage.blob.core.windows.net/videos/2c9bdc9d-6b6f-4823-844c-2a2cd5c36a86/15-May-2021-14-10-637566774755686276.mp4"/>
             </div>
             <div>
               <h3> Letter Shooter </h3>
