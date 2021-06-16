7ef79b0f9eff07a1e86a9c0742d0580af2534194
christiangerbig <chr_gerbig@web.de>
2021-06-16 10:35:22 +0200

    components functions to classes 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,33 +28,3 @@ class NavBar extends Component {
 }
 
 export default NavBar;
-
-
-/*import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../images/tribar-sm.png";
-
-function NavBar(props) {
-  return (
-    <div>
-      <nav>
-        <div className="navbarContainer">
-          <div onClick={props.onHomeClicked}>
-            <Link to={{ pathname: "/" }}> <img src={logo} alt="app logo" className="logo"/> </Link>
-          </div>
-          <div className="navLinkContainer" onClick={props.onAboutClicked}>
-            <Link to={{ pathname: "/about" }} className={(props.menuNumber === 1) ? "navLink textEnlighted" : "navLink"}> About </Link>
-          </div>
-          <div className="navLinkContainer" onClick={props.onProjectsClicked}>
-            <Link to={{ pathname: "/projects" }} className={(props.menuNumber === 2) ? "navLink textEnlighted" : "navLink"} id="projectsLink"> Gallery </Link>
-          </div>
-          <div className="navLinkContainer" onClick={props.onContactClicked}>
-            <Link to={{ pathname: "/contact" }} className={(props.menuNumber === 3) ? "navLink textEnlighted" : "navLink"}> Contact </Link>
-          </div>
-        </div>
-      </nav>
-    </div>
-  )
-}
-
-export default NavBar;*/
