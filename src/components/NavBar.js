24359d85fbc4003cf91183a9104fe8e8996eb644
christiangerbig <chr_gerbig@web.de>
2021-07-16 19:41:07 +0200

    code cleanup 1.3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,13 +11,13 @@ const NavBar = ({menuNumber, onHomeClicked, onAboutClicked, onProjectsClicked, o
             <Link to={{pathname: "/"}}  onClick={onHomeClicked}> <img src={logo} alt="app logo" className="logo"/> </Link>
           </div>
           <div className="navLinkContainer">
-            <Link to={{pathname: "/about"}} className={(menuNumber === 1) ? "navLink textEnlighted" : "navLink"} onClick={onAboutClicked}> About </Link>
+            <Link to={{pathname: "/about"}} className={menuNumber === 1 ? "navLink textEnlighted" : "navLink"} onClick={onAboutClicked}> About </Link>
           </div>
           <div className="navLinkContainer">
-            <Link to={{pathname: "/projects"}} className={(menuNumber === 2) ? "navLink textEnlighted" : "navLink"} id="projectsLink" onClick={onProjectsClicked}> Gallery </Link>
+            <Link to={{pathname: "/projects"}} className={menuNumber === 2 ? "navLink textEnlighted" : "navLink"} id="projectsLink" onClick={onProjectsClicked}> Gallery </Link>
           </div>
           <div className="navLinkContainer">
-            <Link to={{pathname: "/contact"}} className={(menuNumber === 3) ? "navLink textEnlighted" : "navLink"} onClick={onContactClicked}> Contact </Link>
+            <Link to={{pathname: "/contact"}} className={menuNumber === 3 ? "navLink textEnlighted" : "navLink"} onClick={onContactClicked}> Contact </Link>
           </div>
         </div>
       </nav>
