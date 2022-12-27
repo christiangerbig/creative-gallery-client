31a85e267fb6aff39d8f5b9947d3e8fb04246b5e
christiangerbig <chr_gerbig@web.de>
2022-12-27 19:52:33 +0100

    font-face added

--- src/lib/toolsLogos.ts
+++ src/lib/toolsLogos.ts
@@ -11,6 +11,7 @@ import npmLogo from "../images/NPM-Logo.svg";
 import reactLogo from "../images/React-Logo.svg";
 import reduxLogo from "../images/Redux-Logo.svg";
 import sassLogo from "../images/Sass-Logo.svg";
+import tailwindCssLogo from "../images/Tailwind-css-Logo.svg";
 import typeScriptLogo from "../images/TypeScript-Logo.svg";
 import visualStudioCodeLogo from "../images/Visual-Studio-Code-Logo.svg";
 import vueJsLogo from "../images/Vue.js-Logo.svg";
@@ -18,24 +19,25 @@ import webstormLogo from "../images/Webstorm-Logo.svg";
 import yarnLogo from "../images/Yarn-Logo.svg";
 
 const toolsLogos: string[] = [
-    reactLogo,
-    reduxLogo,
-    vueJsLogo,
-    nodeJsLogo,
-    javaScriptES6Logo,
-    typeScriptLogo,
-    sassLogo,
-    cypressLogo,
-    bootstrapLogo,
-    bulmaLogo,
-    mongoDBLogo,
-    cloudinaryLogo,
-    npmLogo,
-    yarnLogo,
-    gitHubLogo,
-    gitLabLogo,
-    visualStudioCodeLogo,
-    webstormLogo
-  ];
+  reactLogo,
+  reduxLogo,
+  vueJsLogo,
+  nodeJsLogo,
+  javaScriptES6Logo,
+  typeScriptLogo,
+  cypressLogo,
+  sassLogo,
+  tailwindCssLogo,
+  bootstrapLogo,
+  bulmaLogo,
+  mongoDBLogo,
+  cloudinaryLogo,
+  npmLogo,
+  yarnLogo,
+  gitHubLogo,
+  gitLabLogo,
+  visualStudioCodeLogo,
+  webstormLogo,
+];
 
 export default toolsLogos;
